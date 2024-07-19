0caca580ecb10e60c8e3f1458562c8592f8b4e7f
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-07-19 16:50:16 -0400

    adding class to load properties

--- test.py
+++ test.py
@@ -2,20 +2,20 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts import  HumanMessagePromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
-
+from LoadProperties import LoadProperties
 from langchain_community.llms import OCIGenAI
 import oci
-
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Step 1 - setup OCI Generative AI llm
 
-# service endpoint
-endpoint = "https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
+properties = LoadProperties()
 
 # use default authN method
 llm = OCIGenAI(
     model_id="cohere.command",
-    service_endpoint="https://inference.generativeai.us-chicago-1.oci.oraclecloud.com",
-    compartment_id="ocid1.user.oc1..aaaaaaaasmynpjr3xdbzlouj7fk4whl5kxwmzjwwny45tostkhdsgdq2hy5a",
+    service_endpoint=properties.getModelName(),
+    compartment_id=properties.getCompartment(),
     model_kwargs={"max_tokens":200},
 )
 
